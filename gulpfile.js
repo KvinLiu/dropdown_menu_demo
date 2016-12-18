c4f3d3ce9cd9457b8e08bba9c65ac58030baf039
Kevin Liu <kevin07liu@gmail.com>
2016-12-18 11:42:08 +0800

    All live editing thing are settled

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp'),
-    pug = require('gulp-pug');
+    pug = require('gulp-pug'),
+    browserSync = require('browser-sync'),
+    rename = require('gulp-rename'),
+    reload  = browserSync.reload;
 
 gulp.task('pug', function(){
-  return gulp.src('./*pug')
+  return gulp.src('src/*.pug')
     .pipe(pug())
+    .pipe(rename('index.html'))
     .pipe(gulp.dest('./'))
+})
+
+gulp.task('browserSync', ['pug'], function(){
+  browserSync({
+    server: {
+      baseDir: './'
+    },
+    notify: false
+  });
+})
+
+gulp.task('default',['pug', 'browserSync'], function(){
+  gulp.watch('src/*.pug',[reload]);
 })
